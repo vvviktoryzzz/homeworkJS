35cf9bb8750567f0ae976cfeef58bc26426d983e
vvviktoryzzz <viktoriabobr929@gmail.com>
2022-05-23 20:04:13 +0300

    upgrade v2.0

--- homework 9/homework 9.js
+++ homework 9/homework 9.js
@@ -1,61 +1,35 @@
 //задание 1
-var arr = [-1, 0, 2, 34, -2];
-
-var filterNumbersArr = arr.filter(function(number) {
+function filterNumbersArr(arr){
+  return arr.filter(function(number) {
   return number > 0;
 });
-
-alert(filterNumbersArr);
+}
+console.log(filterNumbersArr([-1, 0, 2, 34, -2]));
 
 
 // задание 2
-var arr = [1, 0, 2, 3, -4, -2];
-
-var filterNumbersArr = arr.find(function(number) {
+function findNumbersArr(arr){
+ return arr.find(function(number) {
   return number > 0;
 });
-
-alert(filterNumbersArr );
+}
+ console.log(findNumbersArr([1,5,-8,6,-9]));
 
 
 //задание 3
-function checkPalindrome(randomStr){
-    
-       var str = randomStr.toLowerCase();
-       var tmp = 0;
-    
-    
-      if ((str.length) % 2 === 0) {
-        tmp = (str.length) / 2;
-      } else {
+function isPalindrome(s) {
+  var tmp = s.toLowerCase();
+  return tmp == tmp.split("").reverse().join("");
+}
 
-        if (str.length === 1) {
-          return true;
-        } else {
-
-              tmp = (str.length - 1) / 2;
-        }
-      }
     
-      for (var x = 0; x < tmp; x++) {
-    
-        if (str[x] != str.slice(-1-x)[0]) {
-          return false;
-        }
-      }
-      return true;
-    }
-    console.log(checkPalindrome('шалаш'));   
-    console.log(checkPalindrome('шалаШ'));
-    console.log(checkPalindrome('привет'));
+    console.log(isPalindrome('шалаш'));   
+    console.log(isPalindrome('шалаШ'));
+    console.log(isPalindrome('привет'));
 
 
     //задание 4
     function areAnagrams(str1, str2){
-        if(str1.length !== str2.length){
-            return false
-        }
-    
         var string1 = str1.toLowerCase().split('').sort().join('');
         var string2 = str2.toLowerCase().split('').sort().join('');
         return string1 === string2
